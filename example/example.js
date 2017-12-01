2350ee2a29658ac6bc5a91bb442d3cad278f5063
javenkn <javenkn7@yahoo.com>
2017-12-01 18:20:28 +0900

    fixes increase and decrease for time input

--- example/example.js
+++ example/example.js
@@ -3,12 +3,6 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 
 ReactDOM.render(
-  React.createElement(DateTime, {
-    viewMode: 'months',
-    dateFormat: 'MMMM',
-    isValidDate: function(current) {
-      return current.isBefore(DateTime.moment().startOf('month'));
-    }
-  }),
-  document.getElementById('datetime')
+	React.createElement(DateTime, {}),
+	document.getElementById('datetime')
 );
